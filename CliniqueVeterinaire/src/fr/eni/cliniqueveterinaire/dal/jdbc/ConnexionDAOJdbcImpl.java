65ca3b46cf4a467c379b034ad941d5d192b87faa
mgautier2017 <mgautier2017@1-UC39-14.ad.campus-eni.fr>
2018-02-26 16:46:03 +0100

    erter

--- CliniqueVeterinaire/src/fr/eni/cliniqueveterinaire/dal/jdbc/ConnexionDAOJdbcImpl.java
+++ CliniqueVeterinaire/src/fr/eni/cliniqueveterinaire/dal/jdbc/ConnexionDAOJdbcImpl.java
@@ -17,7 +17,7 @@ public class ConnexionDAOJdbcImpl {
 		
 	}
 	
-	public boolean checkConnexion(Personnels personnel){
+	public boolean checkConnexion(Personnels personnel) throws DALException{
 		Connection cnx = null;
 		PreparedStatement rqt = null;
 		ResultSet rs = null;
@@ -44,7 +44,12 @@ public class ConnexionDAOJdbcImpl {
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-			cnx.close();
+			try {
+				cnx.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		
 		return bRes;
