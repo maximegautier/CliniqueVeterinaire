4a046e8555275f167e1960169c8cef6108ad1028
edupuis2017 <edupuis2017@1-UC38-08.ad.campus-eni.fr>
2018-02-27 16:46:24 +0100

    Races DAO : En cours Races JDBC : En cours

---
+++ CliniqueVeterinaire/src/fr/eni/cliniqueveterinaire/dal/RacesDAO.java
@@ -0,0 +1,13 @@
+package fr.eni.cliniqueveterinaire.dal;
+
+import java.util.List;
+
+import fr.eni.cliniqueveterinaire.bo.Races;
+
+/* Crée par Erwin DUPUIS */
+public interface RacesDAO 
+{
+    public List<Races> SelectRacesChat();
+    
+    public List<Races> SelectRacesChien();
+}
---
+++ CliniqueVeterinaire/src/fr/eni/cliniqueveterinaire/dal/jdbc/RacesDAOJdbcImpl.java
@@ -0,0 +1,44 @@
+package fr.eni.cliniqueveterinaire.dal.jdbc;
+
+import java.util.List;
+
+import fr.eni.cliniqueveterinaire.bo.Races;
+import fr.eni.cliniqueveterinaire.dal.RacesDAO;
+
+public class RacesDAOJdbcImpl implements RacesDAO
+{
+    //region DECLARATION
+
+
+    //endregion DECLARATION
+
+    //region CTOR
+
+
+    //endregion CTOR
+    
+    //region METHODS
+    
+	@Override
+	public List<Races> SelectRacesChat() 
+	{
+		
+		return null;
+	}
+
+	@Override
+	public List<Races> SelectRacesChien() 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}	
+    
+    //endregion METHODS
+
+    //region GET/SET
+
+
+    //endregion GET/SET
+
+
+}
